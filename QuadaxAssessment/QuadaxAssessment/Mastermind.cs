a4b48feb9b27474d6bb0e38676d9d156e337d553
dzetzer <dillon.zetzer@sbcglobal.net>
2019-12-18 11:43:28 -1100

    first commit, remade repo to fix gitignore, generates answer

---
+++ QuadaxAssessment/QuadaxAssessment/Mastermind.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuadaxAssessment
+{
+    class Mastermind
+    {
+        public int Attempts { get; set; }
+        public string Answer { get; set; }
+
+        public Mastermind()
+        {
+            Attempts = 10;
+            Answer = GenAnswer();
+        }
+
+        private string GenAnswer()
+        {
+            Random random = new Random();
+            string answer = "";
+
+            for(int i = 0; i <= 3; i++)
+            {
+                answer += random.Next(1, 6).ToString();
+            }
+
+            return answer;
+        }
+    }
+}
---
+++ QuadaxAssessment/QuadaxAssessment/Program.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuadaxAssessment
+{
+    class Program
+    {
+        static void Main()
+        {
+            Mastermind mastermind = new Mastermind();
+        }
+    }
+}
